b4a3501b79945711f9da7a81b58ef153a29b4ecc
Nikola Popovic <nikola.popovic@htecgroup.com>
2021-10-11 19:38:22 +0200

    xcodeproj build

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace TestCICD.xcodeproj \
+xcodebuild -xcodeproj TestCICD.xcodeproj \
             -scheme TestCICD\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
