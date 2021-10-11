7a824a41f05a5115e956a79eb9a4d61857ffc48e
Nikola Popovic <nikola.popovic@htecgroup.com>
2021-10-11 21:20:13 +0200

    change test iOS

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace TestCICD.xcworkspace \
             -scheme TestCICD \
-            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=14.5,name=iPhone\ 11 \
             clean test | xcpretty
