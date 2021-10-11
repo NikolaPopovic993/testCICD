af2f9a5893dcb5f4b32f02730f031f9ee063a3d5
Nikola Popovic <nikola.popovic@htecgroup.com>
2021-10-11 19:37:01 +0200

    add file xcodeproj

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace TestCICD.xcworkspace \
+xcodebuild -workspace TestCICD.xcodeproj \
             -scheme TestCICD\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
