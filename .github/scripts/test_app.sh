e85a67fe94a013b5d79dd5ade9b086c0358bc370
Nikola Popovic <nikola.popovic@htecgroup.com>
2021-10-11 19:42:39 +0200

    xcworkspace and add pod init

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -xcodeproj TestCICD.xcodeproj \
+xcodebuild -workspace TestCICD.xcworkspace \
             -scheme TestCICD\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
